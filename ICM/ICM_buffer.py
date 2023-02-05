824215a2ffc60adfc4c56616868a72abcb55471d
denmanorwat <mr.denivs@yandex.ru>
2023-02-05 11:22:45 +0300

    Backup before cleaning

--- ICM/ICM_buffer.py
+++ ICM/ICM_buffer.py
@@ -4,29 +4,33 @@ import pickle
 from torch import nn
 from threading import Lock
 from Config.ENV_CFG import DEVICE
-
+from Config.A2C_CFG import NUM_AGENTS, NUM_STEPS
 
 class ICMBuffer():
     
-    def __init__(self, sample_size, buffer_size):
-        self.sample_size = sample_size
-        self.buffer_size = buffer_size
+    def __init__(self, sample_size = None, buffer_size = None, save_latest = True):
+        if save_latest:
+            self.buffer_size, self.sample_size = NUM_STEPS*NUM_AGENTS, NUM_STEPS*NUM_AGENTS
+        else:
+            self.sample_size = sample_size
+            self.buffer_size = buffer_size
         self.save_as_train = True
+        self.save_latest = save_latest
         self.buffer = []
-        #self.lock = Lock()
 
     def add_triplet(self, observation, action, next_observation):
-        #print(observation.shape)
+        for i in range(observation.shape[0]):
+            # Observations in format [0; 255] AND int
+            if len(self.buffer) < self.buffer_size:
+                
+                self.buffer.append((observation[i], action[i], next_observation[i]))
 
-        if len(self.buffer) < self.buffer_size:
-            self.buffer.append((observation, action, next_observation))
-        #else:
-        #    if self.save_as_train:
-        #        self.__save_as_train()
-        #        self.save_as_train=False
-        else:
-            swap_index = np.random.choice(self.buffer_size)
-            self.buffer[swap_index] = (observation, action, next_observation)
+            elif not self.save_latest:
+                swap_index = np.random.choice(self.buffer_size)
+                self.buffer[swap_index] = (observation[i], action[i], next_observation[i])
+            elif self.save_latest:
+                del self.buffer[0]
+                self.buffer.append((observation[i], action[i], next_observation[i]))
 
     def get_triplets(self):
         indicies = np.random.choice(len(self.buffer), size = self.sample_size)
@@ -37,10 +41,11 @@ class ICMBuffer():
             previous_observations.append(previous_observation)
             actions.append(action)
             next_observations.append(next_observation)
-        previous_observations = torch.cat(previous_observations, dim=0)
+        previous_observations = torch.stack(previous_observations)
         actions = torch.tensor(actions).to(DEVICE).to(torch.long)
-        next_observations = torch.cat(next_observations, dim=0)
+        next_observations = torch.stack(next_observations)
         return previous_observations, actions, next_observations
+
 
     #def __save_as_train(self):
     #    path_to_file = "/home/dvasilev/mario_icm/debug/train_set/pickled_train_list.pkl"
